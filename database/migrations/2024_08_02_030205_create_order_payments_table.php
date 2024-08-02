f7348db6003d4578e56423c27a730d8078dd740b
FerdianFy13 <ferdianfy13@gmail.com>
2024-08-02 10:14:26 +0700

    |FIX| update schema for order payment

---
+++ database/migrations/2024_08_02_030205_create_order_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained('courses');
+            $table->foreignId('user_id')->constrained('users');
+            $table->enum('status', ['Paid', 'Unpaid'])->default('Unpaid');
+            $table->string('transaction_code')->nullable()->unique();
+            $table->string('payment_method')->nullable();
+            $table->string('account_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_payments');
+    }
+};
