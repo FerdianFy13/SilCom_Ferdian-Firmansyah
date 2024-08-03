cbb3618356dcb6be9d8d7d30ca488060c69cb0b4
FerdianFy13 <ferdianfy13@gmail.com>
2024-08-03 10:37:12 +0700

    |FIX| update dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -20,18 +20,21 @@ class DashboardController extends Controller
             SELECT
                 (SELECT COUNT(*) FROM users WHERE id != :userId) AS user_count,
                 (SELECT COUNT(*) FROM courses) AS course_count,
-                (SELECT COUNT(*) FROM categories) AS category_count
+                (SELECT COUNT(*) FROM categories) AS category_count,
+                (SELECT COUNT(*) FROM order_payments WHERE status = 'Paid') AS order_count
         "), ['userId' => $userId]);
 
         $userData = 0;
         $courseData = 0;
         $categoryData = 0;
+        $orderData = 0;
 
         if (!empty($results)) {
             $result = $results[0];
             $userData = $result->user_count ?? 0;
             $courseData = $result->course_count ?? 0;
             $categoryData = $result->category_count ?? 0;
+            $orderData = $result->order_count ?? 0;
         }
 
         return view('pages.dashboard.index', [
@@ -39,7 +42,8 @@ class DashboardController extends Controller
             'menu' => 'Dashboard',
             'userData' => $userData,
             'courseData' => $courseData,
-            'categoryData' => $categoryData
+            'categoryData' => $categoryData,
+            'orderData' => $orderData
         ]);
     }
 }
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -71,7 +71,7 @@
                         <div class="col col-stats ms-3 ms-sm-0">
                             <div class="numbers">
                                 <p class="card-category">Order</p>
-                                <h4 class="card-title">576</h4>
+                                <h4 class="card-title">{{ $orderData }}</h4>
                             </div>
                         </div>
                     </div>
